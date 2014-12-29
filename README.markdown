a0e5ed065d7a5503bb2f1bfa2f19b8fca8bd6265
Markus Olsen <molsen13@gmail.com>
2014-12-28 19:16:18 -0700

    Add linked list exercise and tests.

---
+++ README.markdown
@@ -0,0 +1,3 @@
+This exercise is an attempt at creating a linked list without using Ruby's built-in data storage structures.
+
+This is a work-in-process.
---
+++ linked_list.rb
@@ -0,0 +1,98 @@
+#require 'pry'
+class IterativeLinkedList
+	attr_accessor :head, :tail
+	def initialize
+		@head = nil
+		@tail = nil
+		@list_count = 0
+	end
+
+	def count
+		if @head.nil?
+			0
+		else
+			@list_count
+		end
+	end
+
+	def push(data)
+		@list_count += 1
+		node = Node.new(data)
+		if @head.nil?
+			# if there is only one item in list, then it is both
+			# the head and tail, so they are set equal to each other
+		  @head = node
+		  @tail = @head
+		else
+			# if head has a value and node.next is nil, 
+			@tail.next = node
+			@tail = node
+		end
+	end
+
+	def pop
+		current = @head
+		return nil if current.nil?
+		@list_count -= 1
+		popped_tail = @tail.data
+		if @head.next == nil
+			# if only one item in list, set it equal to nil
+			# after pop has been called
+			@head = nil
+			@tail = @head
+			popped_tail
+		else 
+			head = @head
+			head = head.next until head.next.next == nil
+			@tail = head
+
+			@tail.next = nil
+			popped_tail
+		end
+	end
+
+	def delete(data)
+		current = @head
+		last = @tail
+		return nil if current.nil?
+		if current.data == data
+			@head = @head.next
+			@list_count -= 1
+		elsif last.data == data
+			@tail = nil
+			@list_count -= 1
+		else
+		  while current.next != nil
+
+		  	if current.next.data == data && current.next.next == nil
+		  		current.next = nil
+		  		@list_count -= 1
+		  	elsif current.next.data == data
+		  		current.next = current.next.next
+		  		@list_count -= 1
+		  	else
+		  		break
+		  	end
+		  end
+		end
+	end
+end
+
+class Node
+  attr_accessor :data, :next
+  def initialize(data)
+  	@data = data
+  	@next = nil
+  end
+end
+list = IterativeLinkedList.new
+list.push("hello")
+list.push("world")
+list.push("today")
+# list.push("christmas")
+# list.push("heythere")
+# p list.pop
+# p list.tail
+p list 
+list.delete("world")
+p list 
